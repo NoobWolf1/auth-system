ce88b1d0bc1bd2af6d047acfb7bb70d48916ba0f
NoobWolf1 <malayshukla21@gmail.com>
2025-06-19 22:28:29 +0530

    basic functionality is working in dev mode

--- src/utils/error.js
+++ src/utils/error.js
@@ -1,9 +1,12 @@
 class AppError extends Error {
-  constructor(message, statusCode = 500, isOperational = true) {
+  // Added 'errors' parameter to the constructor
+  constructor(message, statusCode = 500, isOperational = true, errors = null) {
     super(message);
     this.statusCode = statusCode;
     this.isOperational = isOperational;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    // Store the errors array if provided
+    this.errors = errors; 
 
     Error.captureStackTrace(this, this.constructor);
   }
